89f6c1ea8f8ed6ed6446af0a1fb565ea2c6a92e0
MarcoVacchi <mvacchi816@gmail.com>
2025-04-03 15:54:39 +0200

    feat: create icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,8 @@
+import imageIconOne from "../assets/img/buy-comics-digital-comics.png";
+import imageIconTwo from "../assets/img/buy-comics-merchandise.png";
+import imageIconThree from "../assets/img/buy-comics-shop-locator.png";
+import imageIconFour from "../assets/img/buy-comics-subscriptions.png";
+import imageIconFive from "../assets/img/buy-dc-power-visa.svg";
 function Main() {
 
     return (
@@ -8,8 +13,11 @@ function Main() {
                 </div>
                 <div class="container-main">
                     <ul class="icon-list">
-                        <li class="icon">a</li>
-
+                        <li class="icon"><img src={imageIconOne} alt="icon-1" class="img-icon" /><span>DIGITAL COMICS</span></li>
+                        <li class="icon"><img src={imageIconTwo} alt="icon-2" class="img-icon" />DC MERCHANDISE</li>
+                        <li class="icon"><img src={imageIconThree} alt="icon-3" class="img-icon" />SUBSCRIPTION</li>
+                        <li class="icon"><img src={imageIconFour} alt="icon-4" class="img-icon" />COMIC SHOP LOCATOR</li>
+                        <li class="icon"><img src={imageIconFive} alt="icon-5" class="img-icon-svg" />DC POWER VISA</li>
                     </ul>
                 </div>
             </main>
