a1974e32f3146a239d5647a03a014a0c9a57a277
MarcoVacchi <mvacchi816@gmail.com>
2025-04-03 15:36:38 +0200

    feat: started main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,17 @@ function Header() {
                     <Logo />
                 </figure>
                 <nav>
-                    <ul>
-                        <li>CHARACTERS</li>
-                        <li>COMICS</li>
-                        <li>MOVIES</li>
-                        <li>TV</li>
-                        <li>GAMES</li>
-                        <li>COLLECTIBLES</li>
-                        <li>VIDEOS</li>
-                        <li>FANS</li>
-                        <li>NEWS</li>
-                        <li>SHOP</li>
+                    <ul class="ul-class">
+                        <li class="margin">CHARACTERS</li>
+                        <li class="margin">COMICS</li>
+                        <li class="margin">MOVIES</li>
+                        <li class="margin">TV</li>
+                        <li class="margin">GAMES</li>
+                        <li class="margin">COLLECTIBLES</li>
+                        <li class="margin">VIDEOS</li>
+                        <li class="margin">FANS</li>
+                        <li class="margin">NEWS</li>
+                        <li class="margin">SHOP</li>
                     </ul>
                 </nav>
             </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,15 @@ function Main() {
     return (
         <>
             <main>
-                <h1>Main</h1>
+                <div class="container-h1">
+                    <h1>Content goes here</h1>
+                </div>
+                <div class="container-main">
+                    <ul class="icon-list">
+                        <li class="icon">a</li>
+
+                    </ul>
+                </div>
             </main>
 
         </>
