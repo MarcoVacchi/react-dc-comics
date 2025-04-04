ba56a4d06d3719298d73d334264f5621c0da839c
MarcoVacchi <mvacchi816@gmail.com>
2025-04-04 16:17:44 +0200

    feat: added map method

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,16 +13,11 @@ function Header(props) {
                 </figure>
                 <nav>
                     <ul className="ul-class">
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[0]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[1]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[2]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[3]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[4]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[5]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[6]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[7]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[8]}</a></li>
-                        <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[9]}</a></li>
+                        {linkChild.map((item, index) => (
+                            <li key={index} className="margin hover-text">
+                                <a href="#" className="no-decore">{item}</a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </header>
@@ -32,5 +27,14 @@ function Header(props) {
 
 export default Header;
 
-
-
+// codice a mano
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[0]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[1]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[2]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[3]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[4]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[5]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[6]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[7]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[8]}</a></li>
+// <li className="margin hover-text"><a href="#" className="no-decore">{linkChild[9]}</a></li>
